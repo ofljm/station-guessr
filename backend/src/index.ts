f7b4119d3ee9bedbacef41a80b7d53c6c8f1d5ff
SquigglyRoman <rohen04@gmail.com>
2024-11-11 20:20:09 +0100

    Setup cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,18 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const allowedOrigins = ['https://station-guessr-frontend.onrender.com', 'http://localhost:5173'];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
+app.use(cors(options));
+
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, TypeScript Express!');
