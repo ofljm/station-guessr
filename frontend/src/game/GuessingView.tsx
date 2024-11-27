9ef257687323f3ba7103190198c93840cb43385b
SquigglyRoman <rohen04@gmail.com>
2024-11-27 20:22:43 +0100

    Allow submitting by typing; debounce 300ms

--- frontend/src/game/GuessingView.tsx
+++ frontend/src/game/GuessingView.tsx
@@ -1,3 +1,4 @@
+import { Box, Button, Input } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { Api } from '../api/Api';
 import { GameSession } from '../domain/PlayerSession';
@@ -12,6 +13,8 @@ const GuessingView: React.FC<GuessingViewProps> = ({ gameSession, token }) => {
     const [correctlyGuessedStationNames, setCorrectlyGuessedStationNames] = useState<string[]>(gameSession.correctlyGuessedStationNames);
     const [currentGuess, setCurrentGuess] = useState<string | undefined>();
     const [guessResult, setGuessResult] = useState<string | undefined>();
+    const [typingTimeout, setTypingTimeout] = useState<number | undefined>();
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
     useEffect(() => {
         tickTime(gameSession, setTimeRemaining);
@@ -25,6 +28,19 @@ const GuessingView: React.FC<GuessingViewProps> = ({ gameSession, token }) => {
         return () => clearInterval(timer);
     }, []);
 
+    useEffect(() => {
+        console.log(typingTimeout);
+        if (typingTimeout) {
+            clearTimeout(typingTimeout);
+        }
+
+        setTypingTimeout(setTimeout(() => {
+            handleGuess();
+        }, 300));
+
+        return () => clearTimeout(typingTimeout);
+    }, [currentGuess]);
+
     function tickTime(gameSession: GameSession, setTimeRemaining: React.Dispatch<React.SetStateAction<number>>) {
         const elapsed = (Date.now() - gameSession.startTime) / 1000;
         const timeRemaining = Math.max(0, gameSession.duration - elapsed);
@@ -32,9 +48,15 @@ const GuessingView: React.FC<GuessingViewProps> = ({ gameSession, token }) => {
     }
 
     async function handleGuess() {
+        if (isSubmitting) {
+            return;
+        }
+
         if (!currentGuess) {
             return;
         }
+
+        setIsSubmitting(true);
 
         Api.submitGuess(token, currentGuess)
             .then(response => {
@@ -44,24 +66,30 @@ const GuessingView: React.FC<GuessingViewProps> = ({ gameSession, token }) => {
             .catch(error => {
                 setGuessResult('There was some technical error while submitting the guess. Please try again or contact support.');
                 console.error('Guess failed:', error);
-            });
+            })
+            .finally(() => setIsSubmitting(false));
     }
 
     return (
         <>
             <p>Time remaining: {timeRemaining}</p>
-            <input
-                type="text"
-                placeholder="Your guess"
-                onChange={(e) => setCurrentGuess(e.target.value)}
-            />
-            <button disabled={timeRemaining <= 0} onClick={handleGuess}>Guess</button>
-            {guessResult && <p style={{ color: "blue" }}>{guessResult}</p>}
-            <ul>
-                {correctlyGuessedStationNames.map((station) => (
-                    <li key={station}>{station}</li>
-                ))}
-            </ul>
+            <Box onSubmit={handleGuess}>
+                <Input
+                    type="text"
+                    placeholder="Your guess"
+                    sx={{ mr: 1 }}
+                    onChange={(e) => setCurrentGuess(e.target.value)}
+                />
+                <Button disabled={timeRemaining <= 0 || isSubmitting} onClick={handleGuess} variant="contained" size="medium">
+                    {'Raten'}
+                </Button>
+                {guessResult && <p style={{ color: "blue" }}>{guessResult}</p>}
+                <ul>
+                    {correctlyGuessedStationNames.map((station) => (
+                        <li key={station}>{station}</li>
+                    ))}
+                </ul>
+            </Box >
         </>
     );
 };
