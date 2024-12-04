7142cac7f09b0a100a4632b383349b1f0d832269
SquigglyRoman <rohen04@gmail.com>
2024-12-04 16:44:01 +0100

    Remove almost all basic styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 
